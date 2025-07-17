42692aed2f7106a1443d4061af47a153cf3b5aca
diya <shresthadiya59@gmail.com>
2025-07-17 15:27:08 +0545

    admin customization

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Todolist
 # Register your models here.
+
+@admin.register(Todolist)
+class TodolistAdmin(admin.ModelAdmin):
+   list_display = ('id','title','status')
+   list_filter = ('status',)                 # todolist.objects.filter(status="Yes", title="ndf,sn")
+   search_fields = ('title','description')
+   list_per_page = 5
+   list_editable = ('status',)
+
+# admin.site.register(Todolist, TodolistAdmin)
--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,6 @@ class Todolist(models.Model):
    title = models.CharField(max_length=100)
    description = models.TextField()
    status = models.BooleanField(default=False)
+   
+   def __str__(self):
+      return f"This is the {self.title} - {self.status}"
